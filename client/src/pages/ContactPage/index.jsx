d9f7ccb037349868ff246fc64205c30c33aaf1f6
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2020-05-20 23:08:05 -0400

    added pdf and a new background photo

--- client/src/pages/ContactPage/index.jsx
+++ client/src/pages/ContactPage/index.jsx
@@ -1,126 +1,132 @@
-import React, { Component } from "react";
-import { Modal } from "react-bootstrap";
+import React, {Component} from "react";
+import {Modal} from "react-bootstrap";
 import ContactForm from "../../components/ContactForm";
-import { Redirect } from "react-router-dom";
+import {Redirect} from "react-router-dom";
 import API from "../../lib/API";
 import * as emailjs from 'emailjs-com';
 
 export default class ContactPage extends Component {
-  state = {
-    redirectToReferrer: false,
-    error: "",
-    show: false,
-    errShow: false,
-  };
-
-  componentDidMount(){
-    document.title='Kenan Ozbelli | Contact';
-    document.body.style='';
-};
-
-  handleClose = () => {
-    this.setState({
-      show: false,
-      errShow: false,
-    });
-  };
-
-  handleSubmit = (name, email, message) => {
-    if (!name || !email || !message) {
-      this.setState({
+    state = {
+        redirectToReferrer: false,
+        error: "",
         show: false,
-        errShow: true,
-      });
-      return;
+        errShow: false
+    };
 
-    } else {
+    componentDidMount() {
+        document.title = 'Kenan Ozbelli | Contact';
+        document.body.style = '';
+    };
 
-     
+    handleClose = () => {
+        this.setState({show: false, errShow: false});
+    };
 
-      this.setState({
-        show: true,
-      });
+    handleSubmit = (name, email, message) => {
+        if (!name || !email || !message) {
+            this.setState({show: false, errShow: true});
+            return;
 
-      let templateParams = {
-        from_name: name,
-        to_name:email,
-        name: 'kenan',
-        message_html:message
-      }
-  
-      emailjs.send(
-        'gmail',
-        'template_LYzfi7Zc',
-        templateParams,
-        `user_dcvPcMJFroG6yBnd4rN8z`
-      )
-    
-      setTimeout(() => {
-        API.Contacts.create(name, email, message)
-          .then((response) => {
-            this.setState({ redirectToReferrer: true });
-          })
-          .catch((err) => {
-            if (err.response.status === 401) {
-              this.setState({ error: "Sorry, Please try again." });
+        } else {
+
+
+            this.setState({show: true});
+
+            let templateParams = {
+                from_name: name,
+                to_name: email,
+                name: 'kenan',
+                message_html: message
             }
-          });
-      }, 4000);
+
+            emailjs.send('gmail', 'template_LYzfi7Zc', templateParams, `user_dcvPcMJFroG6yBnd4rN8z`)
+
+            setTimeout(() => {
+                API.Contacts.create(name, email, message).then((response) => {
+                    this.setState({redirectToReferrer: true});
+                }).catch((err) => {
+                    if (err.response.status === 401) {
+                        this.setState({error: "Sorry, Please try again."});
+                    }
+                });
+            }, 4000);
+        }
+    };
+
+    render() {
+        const {from} = this.props.location.state || {
+            from: {
+                pathname: "/"
+            }
+        };
+        const {redirectToReferrer} = this.state;
+
+        if (redirectToReferrer) {
+            return <Redirect to={from}/>;
+        }
+
+        return (
+            <>
+                <ContactForm onSubmit={
+                    this.handleSubmit
+                }/>
+                <Modal show={
+                        this.state.show
+                    }
+                    onHide={
+                        this.handleClose
+                }>
+                    <Modal.Header style={
+                        {
+                            color: "white",
+                            padding: "1rem",
+                            textAlign: "center",
+                            background: "darkGrey",
+                            margin: "1rem"
+                        }
+                    }>
+                        <div style={
+                            {margin: '0 auto'}
+                        }>
+                            <h2>Thank You !!!</h2>
+                        </div>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>
+                            <strong>
+                                Your Response has been recieved and recorded. Thank you for your time, you will be redirected back to the homepage.
+                            </strong>
+                        </p>
+                    </Modal.Body>
+                </Modal>
+                <Modal show={
+                        this.state.errShow
+                    }
+                    onHide={
+                        this.handleClose
+                }>
+                    <Modal.Header style={
+                        {
+                            color: "white",
+                            padding: "1rem",
+                            textAlign: "center",
+                            background: "red",
+                            margin: "1rem"
+                        }
+                    }>
+                        <div style={
+                            {margin: '0 auto'}
+                        }>
+                            <h2>Hey Hold On a Second!</h2>
+                        </div>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>
+                            <strong>Hey, you haven't entered name or email. Please re-enter before submitting this form. Thank you!</strong>
+                        </p>
+                    </Modal.Body>
+                </Modal>
+            </>
+        );
     }
-  };
-
-  render() {
-    const { from } = this.props.location.state || { from: { pathname: "/" } };
-    const { redirectToReferrer } = this.state;
-
-    if (redirectToReferrer) {
-      return <Redirect to={from} />;
-    }
-
-    return (
-      <>
-        <ContactForm onSubmit={this.handleSubmit} />
-        <Modal show={this.state.show} onHide={this.handleClose}>
-          <Modal.Header
-            style={{
-              color: "white",
-              padding: "1rem",
-              textAlign: "center",
-              background: "darkGrey",
-              margin: "1rem",
-            }}
-          >
-            <div style={{margin:'0 auto'}}>
-              <h2>Thank You !!!</h2>
-            </div>
-          </Modal.Header>
-          <Modal.Body>
-            <p>
-              <strong>
-                Your Response has been recieved and recorded. Thank you for your time, you will be redirected back to the homepage. 
-              </strong>
-            </p>
-          </Modal.Body>
-        </Modal>
-        <Modal show={this.state.errShow} onHide={this.handleClose}>
-        <Modal.Header
-            style={{
-              color: "white",
-              padding: "1rem",
-              textAlign: "center",
-              background: "red",
-              margin: "1rem",
-            }}
-          >
-            <div style={{margin:'0 auto'}}>
-              <h2>Hey Hold On a Second!</h2>
-            </div>
-          </Modal.Header>
-          <Modal.Body>
-            <p><strong>Hey, you haven't entered name or email. Please re-enter before submitting this form. Thank you!</strong></p>
-          </Modal.Body>
-        </Modal>
-      </>
-    );
-  }
 }
--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -1,27 +1,41 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import './index.css'
 
-class Home extends Component{
+class Home extends Component {
 
-    componentDidMount(){
-        document.title='Kenan Ozbelli | Home'
-        document.body.style='background: url(https://www.desktopbackground.org/download/o/2015/06/01/957548_charlotte-wallpapers_1920x1080_h.jpg);background-attachment: fixed; min-height: 100%; background-repeat: no-repeat; background-position: center; background-size: cover;'
+    componentDidMount() {
+        document.title = 'Kenan Ozbelli | Home'
+        document.body.style = 'background: url(https://content.fortune.com/wp-content/uploads/2019/07/GettyImages-1020752970-e1563589766704.jpg);background-attachment: fixed; min-height: 100%; background-repeat: no-repeat; background-position: center; background-size: cover;'
     }
-    render(){
+    render() {
 
 
-        return(
+        return (
             <>
-            <h1 className='text-center m-3 animate'>Welcome To My Page</h1>
-            <h2 className='text-center m-3 animate'> I am a certified Full-Stack Web Developer</h2>
-            <h3 className='text-center m-3 animate'> Based in Charlotte, NC</h3>
-           <Link to='/about-me'> <h4 className='text-center m-5 slideLeft'> <button className='exploreButton p-3' style={{border:'none',borderRadius:'.3rem', color:'white'}}>Explore My Page</button></h4></Link>
+                <h1 className='text-center m-3 animate'>Welcome To My Page</h1>
+                <h2 className='text-center m-3 animate'>
+                    I am a certified Full-Stack Web Developer</h2>
+                <h3 className='text-center animate m-3'>
+                    Based in Charlotte, NC</h3>
+                
+                
+                <Link to='/about-me'>
+                    <h4 className='text-center m-5 slideLeft'>
+                        <button className='exploreButton p-3'
+                            style={
+                                {
+                                    border: 'none',
+                                    borderRadius: '.3rem',
+                                    color: 'white'
+                                }
+                        }>Explore My Page</button>
+                    </h4>
+                </Link>
             </>
 
         )
     }
-
 
 
 }
