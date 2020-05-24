b4982df92c94df2eb06342fbcfce71a1d5a3a0b1
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2020-05-24 12:30:12 -0400

    transtition changes

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -14,15 +14,16 @@ class Home extends Component {
 
         return (
             <>
-            <div style={{textDecoration:'none', border:'none'}}>
-                <h1 className='text-center m-3 animate'>Welcome To My Page</h1>
+            <div style={{textDecoration:'none', border:'none'}} className='animate'>
+                <h1 className='text-center m-3'>Welcome To My Page</h1>
             
             
-                <h2 className='text-center m-3 animate'>
+                <h2 className='text-center m-3'>
                     I am a certified Full-Stack Web Developer</h2>
         
-                <h3 className='text-center animate m-3'>
+                <h3 className='text-center m-3'>
                     Based in Charlotte, NC</h3>
+                </div>
                 <Link to='/about-me'>
                     <h4 className='text-center m-5 slideLeft'>
                         <button className='exploreButton p-3'
@@ -35,7 +36,7 @@ class Home extends Component {
                         }>Explore My Page</button>
                     </h4>
                 </Link>
-            </div>
+            
             </>
 
         )
