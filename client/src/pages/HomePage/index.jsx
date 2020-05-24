ffdd48bb897c9e41b7e68f14caf1b6d6ddde20a5
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2020-05-24 11:39:42 -0400

    added div

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -13,19 +13,16 @@ class Home extends Component {
 
 
         return (
-            <div>
-            <div>
+            <>
+            <div style={{textDecoration:'none', border:'none'}}>
                 <h1 className='text-center m-3 animate'>Welcome To My Page</h1>
-            </div>
-            <div>
+            
+            
                 <h2 className='text-center m-3 animate'>
                     I am a certified Full-Stack Web Developer</h2>
-            </div>
-            <div>
+        
                 <h3 className='text-center animate m-3'>
                     Based in Charlotte, NC</h3>
-            </div>
-            <div>
                 <Link to='/about-me'>
                     <h4 className='text-center m-5 slideLeft'>
                         <button className='exploreButton p-3'
@@ -39,7 +36,7 @@ class Home extends Component {
                     </h4>
                 </Link>
             </div>
-            </div>
+            </>
 
         )
     }
